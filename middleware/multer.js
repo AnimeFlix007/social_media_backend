9c92364521321269a5394c71e81b5c9bfc41e98d
AnimeFlix007 <devjitbose111@gmail.com>
2023-05-22 01:40:40 +0530

    filename

--- middleware/multer.js
+++ middleware/multer.js
@@ -5,7 +5,7 @@ const storage = multer.diskStorage({
     cb(null, "./uploads");
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file?.filename);
+    cb(null, Date.now() + "-" + file?.fileName);
   },
 });
 
