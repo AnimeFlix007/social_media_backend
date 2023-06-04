a68a889d50ccdbb66daa66d48302b864144fd3cb
AnimeFlix007 <devjitbose111@gmail.com>
2023-06-04 19:17:29 +0530

    limit

--- controllers/postCtrl.js
+++ controllers/postCtrl.js
@@ -25,7 +25,7 @@ const createPost = async (req, res, next) => {
 const getAllPosts = async (req, res, next) => {
   const userId = req.user._id;
   const page = req.query.page || 1;
-  const limit = 8;
+  const limit = 2;
   const skip = (page - 1) * limit;
   try {
     const posts = await Post.find({})
